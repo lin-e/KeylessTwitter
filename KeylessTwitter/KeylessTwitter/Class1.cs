c46b15a83267729d6ebc784f8c8e3e7d943f406f
Commodity Commodity <commodity@faggot.pw>
2016-02-05 19:38:22 +0000

    Added methods for following, unfollowing and DMs

--- KeylessTwitter/KeylessTwitter/Class1.cs
+++ KeylessTwitter/KeylessTwitter/Class1.cs
@@ -93,7 +93,7 @@ namespace KeylessTwitter
             }
             if (true)
             {
-                HttpWebRequest webRequest = createWebRequest_POST("https://twitter.com/i/tweet/create", new string[][] { }, Encoding.ASCII.GetBytes(string.Format("authenticity_token={0}&is_permalink_page=false&page_context=profile&place_id=&status={1}&tagged_users=&media_ids={2}&tweetboxId=swift_tweetbox_1454534494328", authenticityToken, HttpUtility.UrlEncode(toTweet), mediaID)), "application/x-www-form-urlencoded", true);
+                HttpWebRequest webRequest = createWebRequest_POST("https://twitter.com/i/tweet/create", new string[][] { }, Encoding.ASCII.GetBytes(string.Format("authenticity_token={0}&is_permalink_page=false&page_context=profile&place_id=&status={1}&tagged_users=&media_ids={2}", authenticityToken, HttpUtility.UrlEncode(toTweet), mediaID)), "application/x-www-form-urlencoded", true);
                 using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse()) { }
             }
         }
@@ -121,6 +121,117 @@ namespace KeylessTwitter
                 using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse()) { }
             }
         }
+        public void SendMessage(string targetUser, string messageContents)
+        {
+            if (twitterStatus != TwitterAuthStatus.Success)
+            {
+                throw new InvalidTwitterActionException("You are not signed in");
+            }
+            string conversationID = "";
+            Regex getUserID = new Regex("data-screen-name=\"(.*?)\" data-name=\"(.*?)\" data-user-id=\"(.*?)\"");
+            if (true)
+            {
+                HttpWebRequest webRequest = createWebRequest_GET("https://twitter.com/" + targetUser, new string[][] { }, "");
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    string responseText = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+                    foreach (Match singleMatch in getUserID.Matches(responseText))
+                    {
+                        if (singleMatch.Groups[1].ToString().ToLower() == targetUser.ToLower())
+                        {
+                            conversationID += singleMatch.Groups[3];
+                            break;
+                        }
+                    }
+                    conversationID += "-";
+                }
+            }
+            if (true)
+            {
+                HttpWebRequest webRequest = createWebRequest_GET("https://twitter.com/", new string[][] { }, "");
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    string responseText = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+                    foreach (Match singleMatch in getUserID.Matches(responseText))
+                    {
+                        if (singleMatch.Groups[1].ToString().ToLower() == authUsername.ToLower())
+                        {
+                            conversationID += singleMatch.Groups[3];
+                            break;
+                        }
+                    }
+                }
+            }
+            if (true)
+            {
+                try
+                {
+                    HttpWebRequest webRequest = createWebRequest_POST("https://twitter.com/i/direct_messages/new", new string[][] { }, Encoding.ASCII.GetBytes(
+                    string.Format("authenticity_token={0}&conversation_id={2}&scribeContext%5Bcomponent%5D=tweet_box_dm&tagged_users=&text={1}", authenticityToken, HttpUtility.UrlEncode(messageContents), conversationID)), "application/x-www-form-urlencoded", true);
+                    using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse()) { }
+                }
+                catch
+                {
+                    throw new InvalidTwitterActionException("You may not be able to DM this user");
+                }
+            }
+        }
+        public void Follow(string toFollow)
+        {
+            Regex getUserID = new Regex("data-screen-name=\"(.*?)\" data-name=\"(.*?)\" data-user-id=\"(.*?)\"");
+            string userID = "";
+            if (true)
+            {
+                HttpWebRequest webRequest = createWebRequest_GET("https://twitter.com/" + toFollow, new string[][] { }, "");
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    string responseText = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+                    foreach (Match singleMatch in getUserID.Matches(responseText))
+                    {
+                        if (singleMatch.Groups[1].ToString().ToLower() == toFollow.ToLower())
+                        {
+                            userID = singleMatch.Groups[3].ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+            if (true)
+            {
+                HttpWebRequest webRequest = createWebRequest_POST("https://twitter.com/i/user/follow", new string[][] { }, Encoding.ASCII.GetBytes(string.Format("authenticity_token={0}&challenges_passed=false&handles_challenges=1&user_id={1}", authenticityToken, userID)), "application/x-www-form-urlencoded; charset=UTF-8", true, "https://twitter.com/" + toFollow);
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse()) { }
+
+            }
+            Console.WriteLine(userID);
+        }
+        public void Unfollow(string toFollow)
+        {
+            Regex getUserID = new Regex("data-screen-name=\"(.*?)\" data-name=\"(.*?)\" data-user-id=\"(.*?)\"");
+            string userID = "";
+            if (true)
+            {
+                HttpWebRequest webRequest = createWebRequest_GET("https://twitter.com/" + toFollow, new string[][] { }, "");
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    string responseText = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+                    foreach (Match singleMatch in getUserID.Matches(responseText))
+                    {
+                        if (singleMatch.Groups[1].ToString().ToLower() == toFollow.ToLower())
+                        {
+                            userID = singleMatch.Groups[3].ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+            if (true)
+            {
+                HttpWebRequest webRequest = createWebRequest_POST("https://twitter.com/i/user/unfollow", new string[][] { }, Encoding.ASCII.GetBytes(string.Format("authenticity_token={0}&challenges_passed=false&handles_challenges=1&user_id={1}", authenticityToken, userID)), "application/x-www-form-urlencoded; charset=UTF-8", true, "https://twitter.com/" + toFollow);
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse()) { }
+
+            }
+            Console.WriteLine(userID);
+        }
         public enum TwitterAuthStatus
         {
             Unknown,
@@ -129,7 +240,7 @@ namespace KeylessTwitter
             SigningIn
         }
         #region webRequestFunctions
-        public HttpWebRequest createWebRequest_POST(string finalURL, string[][] requestHeaders, byte[] postData, string contentType, bool isTweet = false)
+        public HttpWebRequest createWebRequest_POST(string finalURL, string[][] requestHeaders, byte[] postData, string contentType, bool isTweet = false, string refererURL = "")
         {
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(finalURL);
             webRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36";
@@ -140,6 +251,10 @@ namespace KeylessTwitter
                 webRequest.Accept = "application/json, text/javascript, */*; q=0.01";
                 webRequest.Referer = "https://twitter.com/";
                 webRequest.Headers.Add("x-requested-with", "XMLHttpRequest");
+            }
+            if (refererURL != "")
+            {
+                webRequest.Referer = refererURL;
             }
             webRequest.ContentType = contentType;
             foreach (string[] headerPair in requestHeaders)
